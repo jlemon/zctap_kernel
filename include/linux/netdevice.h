cd75e04885c474b3b23bcb19e7d0d1317c4cc377
Jonathan Lemon <bsd@fb.com>
2020-07-25 13:27:45 -0700

    netdevice: add SETUP_ZCTAP to the netdev_bpf structure
    This command will be used to setup/tear down zctap queues.
    
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -878,6 +878,7 @@ enum bpf_netdev_command {
 	BPF_OFFLOAD_MAP_ALLOC,
 	BPF_OFFLOAD_MAP_FREE,
 	XDP_SETUP_XSK_POOL,
+	XDP_SETUP_ZCTAP,
 };
 
 struct bpf_prog_offload_ops;
@@ -916,6 +917,11 @@ struct netdev_bpf {
 			struct xsk_buff_pool *pool;
 			u16 queue_id;
 		} xsk;
+		/* XDP_SETUP_ZCTAP */
+		struct {
+			struct zctap_ifq *ifq;
+			u16 queue_id;
+		} zct;
 	};
 };
 
