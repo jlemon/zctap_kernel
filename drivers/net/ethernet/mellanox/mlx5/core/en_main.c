cc660a0041bdcad8411b0308cb4d7d71f1ba3c1d
Jonathan Lemon <jonathan.lemon@gmail.com>
2021-04-21 12:39:24 -0700

    mlx5e: Set resource based on correct parameters
    Select the pool memory model from rq->xsk_pool instead
    of from xsk.
    
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -486,7 +486,7 @@ static int mlx5e_alloc_rq(struct mlx5e_params *params,
 		rq->mkey_be = cpu_to_be32(mdev->mlx5e_res.hw_objs.mkey.key);
 	}
 
-	if (xsk) {
+	if (rq->xsk_pool) {
 		err = xdp_rxq_info_reg_mem_model(&rq->xdp_rxq,
 						 MEM_TYPE_XSK_BUFF_POOL, NULL);
 		xsk_pool_set_rxq_info(rq->xsk_pool, &rq->xdp_rxq);
